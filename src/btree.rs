80a633196e1246b191ddc424f062466e51c74daa
Timothée Haudebourg <author@haudebourg.net>
2020-09-30 21:35:28 +0200

    New allocation scheme.

--- src/btree.rs
+++ src/btree.rs
@@ -1,244 +1,129 @@
 use std::ops::Range;
 use linear_btree::{
-	BTreeMap,
-	BTreeExt,
-	Balance,
-	Item,
-	Node,
+	map::{
+		BTreeMap,
+		BTreeExt
+	},
+	node::{
+		ItemAddr,
+		Node
+	},
 	utils::binary_search_min
 };
 use crate::{
 	Address,
-	AddressRange,
 	AllocationStrategy,
-	AllocationBoundary
 };
 
 pub(crate) trait BTreeFreeMap<T> {
-	fn find_free_range(&mut self, min_len: T, strategy: AllocationStrategy) -> Option<Range<T>>;
+	fn address_of_free_range(&self, len: T, strategy: AllocationStrategy) -> Option<(ItemAddr, T)>;
 
-	fn find_free_range_in(&mut self, min_len: T, strategy: AllocationStrategy, id: usize) -> Option<(Range<T>, Balance)>;
+	fn address_of_free_range_in(&self, id: usize, len: T, strategy: AllocationStrategy) -> Option<(ItemAddr, T)>;
 }
 
-impl<T: Address, const N: usize> BTreeFreeMap<T> for BTreeMap<T, Vec<T>, N> {
+impl<T: Address> BTreeFreeMap<T> for BTreeMap<T, usize> {
 	#[inline]
-	fn find_free_range(&mut self, len: T, strategy: AllocationStrategy) -> Option<Range<T>> {
+	fn address_of_free_range(&self, len: T, strategy: AllocationStrategy) -> Option<(ItemAddr, T)> {
 		match self.root_id() {
-			Some(id) => {
-				match self.find_free_range_in(len, strategy, id) {
-					Some((range, balance)) => {
-						match balance {
-							Balance::Underflow(true) => { // The root is empty.
-								self.set_root_id(self.node(id).child_id_opt(0));
-								self.release_node(id);
-							},
-							_ => ()
-						};
-
-						Some(range)
-					},
-					None => None
-				}
-			},
+			Some(id) => self.address_of_free_range_in(id, len, strategy),
 			None => None
 		}
 	}
 
-	fn find_free_range_in(&mut self, len: T, strategy: AllocationStrategy, id: usize) -> Option<(Range<T>, Balance)> {
-		match free_range_offset_in(self.node(id), len, strategy) {
-			Ok(None) => None,
-			Ok(Some(offset)) => {
-				take_from(self, id, offset)
-			},
-			Err((child_index, child_id)) => {
-				match self.find_free_range_in(len, strategy, child_id) {
-					Some((value, child_balance)) => {
-						let balance = self.rebalance_child(id, child_index, child_balance);
-						Some((value, balance))
-					},
-					None if strategy == AllocationStrategy::BestFit => {
-						let offset = child_index;
-						take_from(self, id, offset)
-					},
-					None => None
+	#[inline]
+	fn address_of_free_range_in(&self, mut id: usize, len: T, strategy: AllocationStrategy) -> Option<(ItemAddr, T)> {
+		loop {
+			match free_range_offset_in(self.node(id), len, strategy) {
+				Ok((offset, actual_len)) => {
+					return Some((ItemAddr::new(id, offset), actual_len))
+				},
+				Err(None) => {
+					return None
+				},
+				Err(Some(child_id)) => {
+					id = child_id;
 				}
 			}
 		}
 	}
 }
 
-#[inline]
-fn take_from<T, const M: usize>(btree: &mut BTreeMap<T, Vec<T>, M>, id: usize, offset: usize) -> Option<(Range<T>, Balance)> where T: Address {
-	let range;
-	let mut item_removed = false;
-
-	let taken = {
-		let node = btree.node_mut(id);
-		match node.item_at_mut_opt(offset) {
-			Some(item) => {
-				let len = item.key;
-				let start = item.value.pop().unwrap();
-				range = start..(start+len);
-
-				if item.value.is_empty() {
-					item_removed = true;
-					Some(node.take(offset))
-				} else {
-					None
-				}
-			},
-			None => return None
-		}
-	};
-
-	if item_removed {
-		btree.set_len(btree.len() - 1);
-	}
-
-	match taken {
-		Some(Ok((item, balance))) => { // removed from a leaf.
-			Some((range, balance))
-		},
-		Some(Err(left_child_id)) => { // removed from an internal node.
-			let left_child_index = offset;
-			let (separator, left_child_balance) = btree.remove_rightmost_leaf_of(left_child_id);
-			btree.node_mut(id).replace(offset, separator);
-			let balance = btree.rebalance_child(id, left_child_index, left_child_balance);
-			Some((range, balance))
-		},
-		None => {
-			Some((range, Balance::Balanced))
-		}
-	}
-}
-
-fn free_range_offset_in<T, const M: usize>(node: &Node<T, Vec<T>, M>, len: T, strategy: AllocationStrategy) -> Result<Option<usize>, (usize, usize)> where T: Address {
+fn free_range_offset_in<T>(node: &Node<T, usize>, len: T, strategy: AllocationStrategy) -> Result<(usize, T), Option<usize>> where T: Address {
 	match node {
 		Node::Internal(node) => {
 			let branches = node.branches();
 			match strategy {
 				AllocationStrategy::FirstFit => {
-					match binary_search_min(branches, &len) {
-						Some(i) => {
-							let b = &branches[i];
-							if b.item.key == len {
-								Ok(Some(i))
-							} else {
-								let j = i+1;
-								if j < branches.len() {
-									let b = &branches[j];
-									Ok(Some(j))
-								} else {
-									Err((i, b.child))
-								}
-							}
-						},
-						None => Ok(Some(0))
-					}
+					panic!("TODO")
 				},
 				AllocationStrategy::WorstFit => {
 					let i = branches.len() - 1;
-					Err((i, branches[i].child))
+					Err(Some(branches[i].child))
 				},
 				AllocationStrategy::BestFit => {
 					match binary_search_min(branches, &len) {
 						Some(i) => {
 							let b = &branches[i];
-							if b.item.key == len {
-								Ok(Some(i))
+							if b.item.key() == &len {
+								Ok((i, *b.item.key()))
 							} else {
-								Err((i, b.child))
+								Err(Some(b.child))
 							}
 						},
 						None => {
-							Ok(Some(0))
+							Ok((0, *branches[0].item.key()))
 						}
 					}
 				}
 			}
 		},
 		Node::Leaf(leaf) => {
-			panic!("TODO")
-		},
-		_ => unreachable!()
+			let items = leaf.items();
+			match strategy {
+				AllocationStrategy::FirstFit => {
+					match binary_search_min(items, &len) {
+						Some(i) => {
+							let item = &items[i];
+							if item.key() == &len {
+								Ok((i, *item.key()))
+							} else {
+								let j = i+1;
+								if j < items.len() {
+									Ok((j, *items[j].key()))
+								} else {
+									Err(None)
+								}
+							}
+						},
+						None => Ok((0, *items[0].key()))
+					}
+				},
+				AllocationStrategy::WorstFit => {
+					let i = items.len() - 1;
+					match items.get(i) {
+						Some(item) if *item.key() >= len => Ok((i, *item.key())),
+						_ => Err(None)
+					}
+				},
+				AllocationStrategy::BestFit => {
+					match binary_search_min(items, &len) {
+						Some(i) => {
+							let item = &items[i];
+							if item.key() == &len {
+								Ok((i, *item.key()))
+							} else {
+								let j = i+1;
+								if j < items.len() {
+									Ok((j, *items[j].key()))
+								} else {
+									Err(None)
+								}
+							}
+						},
+						None => Ok((0, *items[0].key()))
+					}
+				}
+			}
+		}
 	}
 }
-
-// pub(crate) trait BTreeFreeMap<T> {
-// 	fn find_free_range(&self, len: T, strategy: AllocationStrategy, left: Item<T, AllocationBoundary>, right: Item<T, AllocationBoundary>) -> Option<T>;
-//
-// 	fn find_free_range_in(&self, len: T, strategy: AllocationStrategy, left: Item<T, AllocationBoundary>, right: Item<T, AllocationBoundary>, id: usize) -> Option<T>;
-// }
-//
-// impl<T: Address, const N: usize> BTreeFreeMap<T> for BTreeMap<T, AllocationBoundary, N> {
-// 	#[inline]
-// 	fn find_free_range(&self, len: T, strategy: AllocationStrategy, left: Item<T, AllocationBoundary>, right: Item<T, AllocationBoundary>) -> Option<T> {
-// 		match self.root_id() {
-// 			Some(id) => self.find_free_range_in(len, strategy, left, right, id),
-// 			None => {
-// 				if left.value == AllocationBoundary::End {
-// 					Some(left.key..right.key)
-// 				} else {
-// 					None
-// 				}
-// 			}
-// 		}
-// 	}
-//
-// 	fn find_free_range_in(&self, len: T, strategy: AllocationStrategy, left: Item<T, AllocationBoundary>, right: Item<T, AllocationBoundary>, id: usize) -> Option<T> {
-// 		let node = self.node(id);
-// 		match node {
-// 			Node::Internal(node) => {
-// 				let mut result: Option<T> = None;
-//
-// 				for (child_left, child_id, child_right) in node.children_with_separators() {
-// 					let child_left = child_left.cloned().unwrap_or(left);
-// 					let child_right = child_right.cloned().unwrap_or(right);
-//
-// 					let child_span = child_right.key - child_left.key;
-// 					if child_span >= len {
-// 						match self.find_free_range_in(len, strategy, child_left, child_right, child_id) {
-// 							Some(range) => {
-// 								let range_len = range.len();
-// 								match (strategy, result.clone()) {
-// 									(AllocationStrategy::FirstFit, None) => return Some(range),
-// 									(AllocationStrategy::WorstFit, Some(current_range)) if current_range.len() < range_len => result = Some(range),
-// 									(AllocationStrategy::BestFit, Some(current_range)) if current_range.len() > range_len => result = Some(range),
-// 									(_, None) => result = Some(range),
-// 									_ => ()
-// 								}
-// 							},
-// 							None => ()
-// 						}
-// 					}
-// 				}
-//
-// 				result
-// 			},
-// 			Node::Leaf(leaf) => {
-// 				let mut result: Option<T> = None;
-// 				let mut left_item = left;
-// 				for right_item in leaf.items().cloned().chain(Some(right)) {
-// 					if left_item.value == AllocationBoundary::End {
-// 						let range = left_item.key..right_item.key;
-// 						let range_len = range.len();
-// 						if range_len >= len {
-// 							match (strategy, result.clone()) {
-// 								(AllocationStrategy::FirstFit, None) => return Some(range),
-// 								(AllocationStrategy::WorstFit, Some(current_range)) if current_range.len() < range_len => result = Some(range),
-// 								(AllocationStrategy::BestFit, Some(current_range)) if current_range.len() > range_len => result = Some(range),
-// 								(_, None) => result = Some(range),
-// 								_ => ()
-// 							}
-// 						}
-// 					}
-//
-// 					left_item = right_item;
-// 				}
-//
-// 				result
-// 			},
-// 			_ => unreachable!()
-// 		}
-// 	}
-// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 #![feature(min_const_generics)]
 
 use std::ops::Range;
+use slab::Slab;
 use linear_btree::{
-	BTreeMap,
-	Item
+	map::{
+		BTreeMap,
+		BTreeExt
+	},
+	node::Item
 };
 
 mod btree;
@@ -14,26 +18,29 @@ pub struct AllocationFailed;
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum AllocationStrategy {
 	/// Minimises allocation time.
+	///
+	/// # Complexity
+	/// - Allocation: O(1 log n)
+	/// - Free: O(log n)
 	FirstFit,
 
-	/// Minimises fragmentation.
+	/// Minimises external fragmentation.
+	///
+	/// # Complexity
+	/// - Allocation: O(2 log n)
+	/// - Free: O(log n)
 	WorstFit,
 
-	/// Minimises memory usage.
+	/// Minimises memory waste.
+	///
+	/// # Complexity
+	/// - Allocation: O(2 log n)
+	/// - Free: O(log n)
 	BestFit
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub(crate) enum AllocationBoundary<T> {
-	/// Marks the begining of an allocated memory region.
-	Begin(T),
-
-	/// Marks the begining of a free memory region.
-	End(T)
-}
-
 pub trait Address : Copy + Ord + std::ops::Add<Output = Self> + std::ops::Sub<Output = Self> {
-	const START: Self;
+	const ZERO: Self;
 }
 
 pub(crate) trait AddressRange<T> {
@@ -47,131 +54,247 @@ impl<T: Address> AddressRange<T> for std::ops::Range<T> {
 	}
 }
 
-pub struct FreeMap<T: Address, const N: usize> {
-	len: T,
-	strategy: AllocationStrategy,
-	map: BTreeMap<T, Vec<T>, N>,
-	boundaries: BTreeMap<T, AllocationBoundary<T>, N>
+fn index(i: usize) -> Option<usize> {
+	if i == std::usize::MAX {
+		Some(i)
+	} else {
+		None
+	}
 }
 
-impl<T: Address, const N: usize> FreeMap<T, N> {
-	#[inline]
-	pub fn new(len: T, strategy: AllocationStrategy) -> FreeMap<T, N> {
-		let mut map = BTreeMap::new();
-		map.insert(len, vec![T::START]);
+struct Page<T> {
+	len: T
+}
 
-		let mut boundaries = BTreeMap::new();
-		boundaries.insert(T::START, AllocationBoundary::End(len));
-		boundaries.insert(len, AllocationBoundary::Begin(len));
+/// Free region of unknown size.
+#[derive(Copy, Clone)]
+struct FreeRegion<T> {
+	page: usize,
+	offset: T,
+	previous_allocated_region: usize
+}
 
-		FreeMap {
-			len,
-			strategy,
-			map,
-			boundaries
+/// Free regions uniquely indexed and organised in linked-lists of same-size regions.
+struct FreeRegions<T> {
+	regions: Slab<(FreeRegion<T>, usize)>
+}
+
+impl<T> FreeRegions<T> {
+	fn new() -> FreeRegions<T> {
+		FreeRegions {
+			regions: Slab::new()
 		}
 	}
 
-	fn insert_free_range(&mut self, free_range: Range<T>) {
-		self.map.update(free_range.len(), |offsets| {
-			match offsets {
-				Some(mut offsets) => {
-					offsets.push(free_range.start);
-					(Some(offsets), ())
-				},
-				None => {
-					let offsets = vec![free_range.start];
-					(Some(offsets), ())
-				}
-			}
-		});
+	fn pop(&mut self, i: usize) -> (Option<usize>, FreeRegion<T>) {
+		panic!("TODO")
 	}
 
-	fn remove_free_range(&mut self, free_range: Range<T>) {
-		self.map.update(free_range.len(), |offsets| {
-			let mut offsets = offsets.expect("corrupted free map");
-			let i = offsets.iter().position(|o| *o == free_range.start).expect("corrupted free map");
-			offsets.swap_remove(i);
-			if offsets.is_empty() {
-				(None, ())
-			} else {
-				(Some(offsets), ())
-			}
-		});
+	fn push(&mut self, i: Option<usize>) -> (Option<usize>, ()) {
+		panic!("TODO")
+	}
+}
+
+#[derive(Copy, Clone)]
+struct AllocatedRegion<T> {
+	offset: T,
+	len: T
+}
+
+struct AllocatedRegions<T> {
+	regions: Slab<(usize, AllocatedRegion<T>, usize)>,
+	first: usize
+}
+
+impl<T> AllocatedRegions<T> {
+	fn new() -> AllocatedRegions<T> {
+		AllocatedRegions {
+			regions: Slab::new(),
+			first: std::usize::MAX
+		}
 	}
 
-	/// ## Complexity
+	/// Insert a first allocated region.
 	///
-	/// Best case (allocation failed): O(1 * log n)
-	/// Best success case: O(4 * log n)
-	/// Worst case: O(5 * log n)
-	#[inline]
-	pub fn allocate(&mut self, len: T) -> Result<T, AllocationFailed> {
-		match self.map.find_free_range(len, self.strategy) {
-			Some(range) => {
-				self.remove_free_range(range.clone());
-
-				let new_free_range = (range.start+len)..range.end;
-				let new_free_range_len = new_free_range.len();
-				if new_free_range_len > T::START {
-					self.insert_free_range(new_free_range);
-					self.boundaries.insert(range.end, AllocationBoundary::End(new_free_range_len));
-				} else {
-					self.boundaries.remove(&range.end);
-				}
-
-				self.boundaries.remove(&range.start);
-				Ok(range.start)
+	/// Return the second allocated region.
+	fn push_front(&mut self, region: AllocatedRegion<T>) -> Option<AllocatedRegion<T>> where T: Copy {
+		let second = self.first;
+		let n = self.regions.insert((std::usize::MAX, region, second));
+		self.first = n;
+		match index(second) {
+			Some(i) => {
+				self.regions[i].0 = n;
+				Some(self.regions[i].1)
 			},
-			None => Err(AllocationFailed)
+			None => None
 		}
 	}
 
-	/// ## Complexity
+	/// Insert an allocated region after the given index.
 	///
-	/// Best case: O(5 * log n)
-	/// Worst case: O(10 * log n)
+	/// Return the next allocated region.
+	fn insert_after(&mut self, i: usize, region: AllocatedRegion<T>) -> Option<AllocatedRegion<T>> where T: Copy {
+		let next = self.regions[i].2;
+		let n = self.regions.insert((i, region, next));
+		self.regions[i].2 = n;
+		match index(next) {
+			Some(j) => {
+				self.regions[j].0 = n;
+				Some(self.regions[j].1)
+			},
+			None => None
+		}
+	}
+
+	/// Remove the region with the given index and return the allocated region before and after it.
+	fn remove(&mut self, i: usize) -> (Option<AllocatedRegion<T>>, Option<AllocatedRegion<T>>) where T: Copy {
+		let node = self.regions.remove(i);
+
+		let prev = match index(node.0) {
+			Some(j) => {
+				let prev = &mut self.regions[j];
+				prev.2 = node.2;
+				Some(prev.1)
+			},
+			None => None
+		};
+
+		let next = match index(node.2) {
+			Some(j) => {
+				let next = &mut self.regions[j];
+				next.0 = node.0;
+				Some(next.1)
+			},
+			None => None
+		};
+
+		(prev, next)
+	}
+
+	/// Free region starting at the given offset index.
+	fn free(&mut self, page_len: T, i: usize) -> Range<T> where T: Address {
+		let (prev, next) = self.remove(i);
+
+		let free_region_start = match prev {
+			Some(prev) => prev.offset + prev.len,
+			None => T::ZERO
+		};
+
+		let free_region_end = match next {
+			Some(next) => next.offset,
+			None => page_len
+		};
+
+		free_region_start..free_region_end
+	}
+
+	/// Allocate a new free region.
+	///
+	/// Returns the range of the next free region.
+	fn allocate(&mut self, page_len: T, i: Option<usize>, offset: T, len: T) -> Range<T> where T: Address {
+		let region = AllocatedRegion {
+			offset,
+			len
+		};
+
+		let next_region = match i {
+			Some(i) => self.insert_after(i, region),
+			None => self.push_front(region)
+		};
+
+		let next_free_region_start = offset + len;
+		let next_free_region_end = match next_region {
+			Some(region) => region.offset,
+			None => page_len
+		};
+
+		next_free_region_start..next_free_region_end
+	}
+}
+
+/// Alloated region informations.
+pub struct Allocation<T> {
+	/// Page where the allocated region is.
+	pub page: usize,
+
+	/// Offset of the allocated region in the page.
+	pub offset: T,
+
+	/// Size of the allocated region.
+	pub len: T
+}
+
+pub struct FreeMap<T: Address> {
+	/// Prefered allocation strategy.
+	strategy: AllocationStrategy,
+
+	/// Memory pages.
+	pages: Slab<Page<T>>,
+
+	/// Allocated regions.
+	allocated_regions: AllocatedRegions<T>,
+
+	/// Free regions.
+	free_regions: FreeRegions<T>,
+
+	/// Maps a size to a free region of the given size.
+	map: BTreeMap<T, usize>
+}
+
+impl<T: Address> FreeMap<T> {
+	#[inline]
+	pub fn new(strategy: AllocationStrategy) -> FreeMap<T> {
+		FreeMap {
+			strategy,
+			pages: Slab::new(),
+			allocated_regions: AllocatedRegions::new(),
+			free_regions: FreeRegions::new(),
+			map: BTreeMap::new()
+		}
+	}
+
+	/// Add a new empty page.
+	///
+	/// Returns the index used to uniquely identify the page.
+	pub fn new_page(&mut self, len: T) -> usize {
+		self.pages.insert(Page {
+			len
+		})
+	}
+
+	#[inline]
+	pub fn allocate(&mut self, len: T) -> Result<Allocation<T>, AllocationFailed> {
+		if len > T::ZERO {
+			match self.map.address_of_free_range(len, self.strategy) {
+				Some((addr, region_len)) => {
+					let free_regions = &mut self.free_regions;
+					let free_region = self.map.update_at(addr, move |i| free_regions.pop(i));
+
+					let page_len = self.pages[free_region.page].len;
+					let new_free_region_range = self.allocated_regions.allocate(page_len, index(free_region.previous_allocated_region), free_region.offset, len);
+
+					let free_regions = &mut self.free_regions;
+					self.map.update(len, move |i| free_regions.push(i)); // O(log n)
+
+					Ok(Allocation {
+						page: free_region.page,
+						offset: free_region.offset,
+						len
+					})
+				},
+				None => Err(AllocationFailed)
+			}
+		} else {
+			Ok(Allocation {
+				page: 0,
+				offset: T::ZERO,
+				len: T::ZERO
+			})
+		}
+	}
+
 	#[inline]
 	pub fn free(&mut self, offset: T, len: T) {
-		let end = offset + len;
-
-		let mut final_free_region_offset = offset;
-		let mut final_free_region_end = end;
-
-		let left = match self.boundaries.get(&offset) {
-			Some(AllocationBoundary::Begin(len)) => {
-				final_free_region_offset = offset - *len;
-				Some(final_free_region_offset..offset)
-			},
-			None => None,
-			_ => panic!("corrupted free map")
-		};
-
-		let right = match self.boundaries.get(&end) {
-			Some(AllocationBoundary::End(len)) => {
-				final_free_region_end = end + *len;
-				Some(end..final_free_region_end)
-			},
-			None => None,
-			_ => panic!("corrupted free map")
-		};
-
-		let final_free_range = final_free_region_offset..final_free_region_end;
-		let final_free_region_len = final_free_range.len();
-
-		if let Some(left) = left {
-			self.remove_free_range(left);
-			self.boundaries.remove(&offset);
-		}
-
-		if let Some(right) = right {
-			self.remove_free_range(right);
-			self.boundaries.remove(&end);
-		}
-
-		self.boundaries.insert(final_free_range.start, AllocationBoundary::End(final_free_region_len));
-		self.boundaries.insert(final_free_range.end, AllocationBoundary::Begin(final_free_region_len));
-
-		self.insert_free_range(final_free_range);
+		panic!("TODO")
 	}
 }
